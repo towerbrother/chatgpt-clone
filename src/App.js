09d8791545a40371a7878191aa0da2ff89a5b586
towerbrother <giorgio.torre8@gmail.com>
2023-11-20 17:28:02 +0100

    clean up code + setup UI + setup server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const getMessage = () => {};
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <section className='side-bar'>
+        <button>+ New chat</button>
+        <ul className='history'></ul>
+        <nav>
+          <p>Made by Giorgio</p>
+        </nav>
+      </section>
+      <section className='main'>
+        <h1>TowerGPT</h1>
+        <ul className='feed'></ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input />
+            <div id='submit' onClick={getMessage}>
+              ➢
+            </div>
+          </div>
+          <p className='info'>
+            TowerGPT can make mistakes. Consider checking important information.
+          </p>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
